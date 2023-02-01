22b37d1c2441b8d9db7e188fe363663e2a906ae7
6310682692 <6310682692@student.tu.ac.th>
2023-02-01 21:44:10 +0700

    Login Page
    Wait for backend

--- base/views.py
+++ base/views.py
@@ -13,3 +13,17 @@ from django.urls import reverse
 
 def index(request):
     return render(request, 'index.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if request.user.is_superuser:
+                return HttpResponseRedirect(reverse('admin:index'))
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('index'))
