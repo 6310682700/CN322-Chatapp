8741d1395da209a60fec07a85d53cbf7ce8784b7
Bulakorn Junboonmee <69451712+6310682577@users.noreply.github.com>
2023-02-03 13:04:04 +0700

    doing upload(not finished)

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import WebUser
+
+from .models import Post
 
 # Create your views here.
+def uploadpost(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            user = User.objects.get(username=request.user)
+            postdesc = request.POST['desc']
+            posts = Post.objects.create(author=request.user, post_body = postdesc)
+        return render(request,'index.html')
+    else:
+        return redirect("login")
