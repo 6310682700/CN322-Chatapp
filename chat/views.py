d353e9d5a253ffdbc9a730009fae1f7ad8e903f0
6310682700 <69451450+6310682700@users.noreply.github.com>
2023-02-05 21:34:49 +0700

    Merge branch 'main' of https://github.com/6310682700/CN322-SocialNetwork into main

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'chat_base.html')
+    if request.user.is_authenticated:
+        return render(request, 'chat_base.html')
+    else:
+        return HttpResponseRedirect(reverse('login'))
 
 def room(request, room_name):
-    return render(request, 'chatroom.html', {
-        'room_name' : room_name
-    })
+    if request.user.is_authenticated:
+        return render(request, 'chatroom.html', {
+            'room_name' : room_name
+        })
+    else:
+        return HttpResponseRedirect(reverse('login'))
